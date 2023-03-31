fc40871ed2f4fc49a7af0ed8605b0c8360e506f4
Andre-se <ibrahimtanriover64@gmail.com>
2023-03-31 03:19:08 +0200

    v1

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./header.css";
+import logo from "../../assets/images/logo.jpeg";
+import { Link } from "react-router-dom";
+import { nav } from "../../assets/data/data";
+import User from "./User";
+const Header = () => {
+  return (
+    <>
+      <header>
+        <div className="scontainer flex">
+          <div className="logo">
+            <img src={logo} alt="logo" width="100px" />
+          </div>
+          <nav>
+            <ul>
+              {nav.map((link) => (
+                <li key={link.id}>
+                  <Link to={link.url}>{link.text}</Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
+          <div className="account flexCenter">
+            <User/>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
